69025e5aa62974093719e9fae49dca5fcc961ab8
tlesatsawat <112902523+tlesatsawat@users.noreply.github.com>
2025-07-22 00:08:47 +0700

    api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://good-sale-pos-backend.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
